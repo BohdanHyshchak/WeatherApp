dc2dd562c703e4789976aa6483a44494df4bd43d
Bohdan Hyshchak <bogdangisak@gmail.com>
2021-08-30 10:05:27 +0300

    save before changing Retrofit.Instance

--- app/src/main/java/com/example/weather_app/ui/fragments/WeatherFragment.kt
+++ app/src/main/java/com/example/weather_app/ui/fragments/WeatherFragment.kt
@@ -1,5 +1,63 @@
 package com.example.weather_app.ui.fragments
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.bumptech.glide.Glide
+import com.example.weather_app.R
+import com.example.weather_app.api.RetrofitInstance
+import com.example.weather_app.databinding.WeatherFragmentBinding
+import retrofit2.HttpException
+import java.io.IOException
 
-class WeatherFragment : Fragment()
+class WeatherFragment : Fragment() {
+
+    val TAG = "Weather Fragment"
+    lateinit var binding: WeatherFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate view and obtain an instance of the binding class
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.weather_fragment,
+            container,
+            false
+        )
+
+        testImage()
+
+        return binding.root
+    }
+
+    private fun testImage() {
+        lifecycleScope.launchWhenCreated {
+             // Glide.with(this@WeatherFragment).load("http://openweathermap.org/img/w/01d.png").into(binding.ivTestImage)
+            // binding.tvTestTextView.text = "HELLO"
+            val response = try {
+                RetrofitInstance.api.getWeatherForecast("London")
+            } catch (e: IOException) {
+                Log.e(TAG, "IOException")
+                return@launchWhenCreated
+            } catch (e: HttpException) {
+                Log.e(TAG, "HttpException")
+                return@launchWhenCreated
+            }
+            if (response.isSuccessful && response.body() != null) {
+                binding.tvTestTextView.text = "HELLO"
+                // Glide.with(this@WeatherFragment).load("http://openweathermap.org/img/w/${response.body()!!.weather[0].icon}.png").into(binding.ivTestImage)
+            } else {
+                Log.e(TAG, "Response was not successful")
+            }
+        }
+    }
+}
